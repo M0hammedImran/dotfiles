5c91dce8886b65f55ebfe347cd3e420f8f3656ca
Mohammed Imran <52968617+M0hammedImran@users.noreply.github.com>
2021-02-05 09:59:56 +0530

    Updated arch-install.sh, removed symlinks and made a seperate file

--- arch-install.sh
+++ arch-install.sh
@@ -50,27 +50,3 @@ Installing Deno
 "
 curl -fsSL https://deno.land/x/install/install.sh | sh
 
-echo "
-====================
-Setting up symlinks
-====================
-"
-if [ ! -d ~/.config/fish/ ]; then 
-  mkdir -p ~/.config/fish  
-fi
-
-if [ ! -d ~/.config/kitty/ ]; then 
-  mkdir ~/.config/kitty 
-fi
-
-if [ ! -d ~/.ssh ]; then 
-  mkdir ~/.ssh  
-fi
-
-ln -s ~/.dotfiles/.vimrc ~/.vimrc
-ln -s ~/.dotfiles/config.fish ~/.config/fish/config.fish
-ln -s ~/.dotfiles/kitty.conf ~/.config/kitty/kitty.conf
-ln -s ~/.dotfiles/starship.toml ~/.config/starship.toml
-ln -s ~/.dotfiles/config ~/.ssh/config
-ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
-
