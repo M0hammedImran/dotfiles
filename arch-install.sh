ff4a9cb970112fddf1894f127cba3729fa1dc97b
Mohammed Imran <52968617+M0hammedImran@users.noreply.github.com>
2021-02-05 07:45:04 +0530

    Updated arch-install.sh

---
+++ arch-install.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+echo "
+=============================
+Checking For Updates if any.
+=============================
+"
+sudo pacman -Syyu
+
+echo "
+==================================================================
+Installing Kitty, Fish, brave, Nodejs 14.x, exa, bat, base-devel & cmake
+==================================================================
+"
+sudo pacman -S kitty fish nodejs-lts-fermium npm brave base-devel cmake exa bat
+
+echo "
+======================================
+Installing VSCode, Postman, Splatmoji
+======================================
+"
+yay -S visual-studio-code-bin postman-bin splatmoji-git
+
+echo "
+====================
+Installing Vim-Plug
+====================
+"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+echo "
+================
+Installing Rust
+================
+"
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+echo "
+================
+Installing Deno
+================
+"
+curl -fsSL https://deno.land/x/install/install.sh | sh
+
+
+echo "
+====================
+Setting up symlinks
+====================
+"
+ln -s ~/.dotfiles/config.fish ~/.config/fish/config.fish
+ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/starship.toml ~/.config/starship.toml
+ln -s ~/.dotfiles/config ~/.ssh/kitty.conf
+ln -s ~/.dotfiles/kitty.conf ~/.config/kitty/kitty.conf
