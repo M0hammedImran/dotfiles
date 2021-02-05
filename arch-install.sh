816e674e67ac1dbe686a31736c3d24212a9a894b
Mohammed Imran <52968617+M0hammedImran@users.noreply.github.com>
2021-02-05 08:11:03 +0530

    Updated arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -8,11 +8,11 @@ Checking For Updates if any.
 sudo pacman -Syyu
 
 echo "
-==================================================================
-Installing Kitty, Fish, brave, Nodejs 14.x, exa, bat, base-devel & cmake
-==================================================================
+==============================================================================
+Installing Kitty, Fish, brave, Nodejs 14.x, exa, bat, yay, base-devel & cmake
+==============================================================================
 "
-sudo pacman -S kitty fish nodejs-lts-fermium npm brave base-devel cmake exa bat
+sudo pacman -S kitty fish nodejs-lts-fermium npm brave base-devel cmake exa bat yay
 
 echo "
 ======================================
@@ -38,11 +38,17 @@ curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
 
 echo "
 ================
+Installing Starship
+================
+"
+curl -fsSL https://starship.rs/install.sh | bash
+
+echo "
+================
 Installing Deno
 ================
 "
 curl -fsSL https://deno.land/x/install/install.sh | sh
-
 
 echo "
 ====================
@@ -52,5 +58,5 @@ Setting up symlinks
 ln -s ~/.dotfiles/config.fish ~/.config/fish/config.fish
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
 ln -s ~/.dotfiles/starship.toml ~/.config/starship.toml
-ln -s ~/.dotfiles/config ~/.ssh/kitty.conf
+ln -s ~/.dotfiles/config ~/.ssh/config
 ln -s ~/.dotfiles/kitty.conf ~/.config/kitty/kitty.conf
