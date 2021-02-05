bf0de96d854a178da5eb18e130808970e9220a41
Mohammed Imran <52968617+M0hammedImran@users.noreply.github.com>
2021-02-05 08:18:47 +0530

    Updated arch-install.sh

--- arch-install.sh
+++ arch-install.sh
@@ -55,8 +55,20 @@ echo "
 Setting up symlinks
 ====================
 "
-ln -s ~/.dotfiles/config.fish ~/.config/fish/config.fish
+if [ ! -d ~/.config/fish/ ]; then 
+  mkdir -p ~/.config/fish  
+fi
+
+if [ ! -d ~/.config/kitty/ ]; then 
+  mkdir ~/.config/kitty 
+fi
+
+if [ ! -d ~/.ssh ]; then 
+  mkdir ~/.ssh  
+fi
+
 ln -s ~/.dotfiles/.vimrc ~/.vimrc
+ln -s ~/.dotfiles/config.fish ~/.config/fish/config.fish
+ln -s ~/.dotfiles/kitty.conf ~/.config/kitty/kitty.conf
 ln -s ~/.dotfiles/starship.toml ~/.config/starship.toml
 ln -s ~/.dotfiles/config ~/.ssh/config
-ln -s ~/.dotfiles/kitty.conf ~/.config/kitty/kitty.conf
