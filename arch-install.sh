2102d73932e8fc72c6fd3d355677c1176f19871f
Mohammed Imran <52968617+M0hammedImran@users.noreply.github.com>
2021-02-05 08:22:20 +0530

    Updated arch-install.sh and symlinks.sh

--- arch-install.sh
+++ arch-install.sh
@@ -72,3 +72,5 @@ ln -s ~/.dotfiles/config.fish ~/.config/fish/config.fish
 ln -s ~/.dotfiles/kitty.conf ~/.config/kitty/kitty.conf
 ln -s ~/.dotfiles/starship.toml ~/.config/starship.toml
 ln -s ~/.dotfiles/config ~/.ssh/config
+ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
+
